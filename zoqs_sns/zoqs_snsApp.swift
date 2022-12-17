afe06449d4b207d4e292586b2d3f800fd6cfb80a
shimasho1020 <shimasho1020@gmail.com>
2022-12-17 17:23:11 +0900

    Initial Commit

---
+++ zoqs_sns/zoqs_snsApp.swift
@@ -0,0 +1,17 @@
+//
+//  zoqs_snsApp.swift
+//  zoqs_sns
+//
+//  Created by 島田将太郎 on 2022/12/17.
+//
+
+import SwiftUI
+
+@main
+struct zoqs_snsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
