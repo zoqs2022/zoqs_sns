7b9e74cde865c5848fafb17c732980a4868eb65b
shimasho1020 <shimasho1020@gmail.com>
2022-12-17 18:49:47 +0900

    add text

--- zoqs_sns/ContentView.swift
+++ zoqs_sns/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("snsを作ろう！")
             Text("なんでやねん")
+            Text("うんこ")
         }
         .padding()
     }
