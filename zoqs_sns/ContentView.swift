ff5bd61164c3a5cfd762f33c2ab0438853991075
takumi0512 <115136152+takumi0512@users.noreply.github.com>
2022-12-17 17:50:01 +0900

    change2

--- zoqs_sns/ContentView.swift
+++ zoqs_sns/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("snsを作ろう！")
+            Text("なんでやねん")
         }
         .padding()
     }
