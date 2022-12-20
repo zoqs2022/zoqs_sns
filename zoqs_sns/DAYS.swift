6026d149be720e44c36b08bbeee27963934e1d71
shimasho1020 <shimasho1020@gmail.com>
2022-12-21 01:05:03 +0900

    create calendar

--- zoqs_sns/DAYS.swift
+++ zoqs_sns/DAYS.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct DAYS: View {
+    
+    let week: [String] = ["日","月","火","水","木","金","土"]
+    @State var diff: Int = 0
+    let today: Int = Int(Date().DateToString(format: "d"))! - 1
+    
     var body: some View {
-        VStack{
-            ZStack{
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(height: 300)
-                Text("カレンダー")
+        
+        ScrollView{
+            VStack{
+                
+                HStack{
+                    Button(action: {diff -= 1}, label: {Image(systemName: "chevron.left.circle.fill")})
+                    Text(Date().changeMonth(diff: diff).DateToString(format: "yyyy年M月")).fontWeight(.bold)
+                    Button(action: {diff += 1}, label: {Image(systemName: "chevron.right.circle.fill")})
+                }
+                
+                ScrollView{
+                    LazyVGrid(columns: Array(repeating: GridItem(), count: 7), spacing: 8)
+                    {
+                        ForEach(week, id: \.self) {
+                            i in
+                            Text(i).foregroundColor(.indigo).fontWeight(.bold).padding(.top)
+                        }
+                        
+                        let days: [Date] = Date().changeMonth(diff: diff).getAllDays()
+                        let start = days[0].getWeekDay()
+                        let end = start + days.count
+                        
+                        let maxBlock: Int = end <= 35 ? 34 : 41
+                        
+                        ForEach((0...maxBlock), id: \.self) {
+                            index in
+                            VStack(spacing : 4){
+                                if(index >= start && index < end){
+                                    let i = index - start
+                                    if(i == today && diff == 0) {
+                                        ZStack(alignment: .center){
+                                            Circle()
+                                                    .frame(width: 24,
+                                                           height: 24,
+                                                           alignment: .center)
+                                                    .foregroundColor(.indigo)
+                                            Text(days[i].DateToString(format: "d"))
+                                                .foregroundColor(.white)
+                                        }
+                                        RoundedRectangle(cornerRadius: 8)
+                                            .frame(width: 40, height: 40, alignment: .center)
+                                            .foregroundColor(.indigo)
+                                            .opacity(1)
+                                        
+                                    } else {
+                                        Text(days[i].DateToString(format: "d"))
+                                        RoundedRectangle(cornerRadius: 8)
+                                            .frame(width: 40, height: 40, alignment: .center)
+                                            .foregroundColor(.cyan)
+                                            .opacity(0.5)
+                                    }
+                
+                                } else {
+    //                                Text("--")
+                                }
+    //                            RoundedRectangle(cornerRadius: 8)
+    //                                .frame(width: 40, height: 40, alignment: .center)
+    //                                .foregroundColor(.yellow)
+    //                                .opacity(0.5)
+                            }
+                        }
+                    }
+                }
             }
+            .padding(.bottom, 30)
 
             ZStack{
                 Rectangle()
---
+++ zoqs_sns/DateExtensionFile.swift
@@ -0,0 +1,57 @@
+//
+//  DateExtensionFile.swift
+//  zoqs_sns
+//
+//  Created by 島田将太郎 on 2022/12/20.
+//
+
+import Foundation
+
+extension Date{
+    
+    func firstDayOfTheMounth() -> Date{
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year,.month], from: self))!
+    }
+    
+    mutating func plusOneDay() {
+        self = Calendar.current.date(byAdding: .day, value: 1, to: self)!
+    }
+    
+    func changeMonth(diff: Int) -> Date {
+        return Calendar.current.date(byAdding: .month, value: diff, to: self)!
+    }
+    
+    func getAllDays() -> [Date] {
+        var day1st = firstDayOfTheMounth()
+
+        // Dataの空の配列
+        var days = [Date]()
+
+        // 配列に要素を一つ追加する
+        days.append(day1st)
+
+        let range = Calendar.current.range(of: .day, in: .month, for: day1st)!
+
+        for _ in 0..<range.count - 1 {
+            day1st.plusOneDay()
+            days.append(day1st)
+        }
+        
+        return days
+    }
+    
+    // 曜日を数値で返す（0が日曜、6が土曜）
+    func getWeekDay() -> Int {
+        return Calendar.current.component(.weekday, from: self) - 1
+    }
+    
+    func DateToString(format:String) -> String {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ja_JP")
+        df.timeZone = TimeZone.current
+        df.dateFormat = format
+        
+        return df.string(from: self)
+    }
+    
+}
