f8f57ef947493f06e1dbfeb9fb80be22af2f8fbe
shimasho1020 <shimasho1020@gmail.com>
2023-02-05 17:15:23 +0900

    delete

--- zoqs_sns/View/SNS/SNS.swift
+++ zoqs_sns/View/SNS/SNS.swift
@@ -16,17 +16,8 @@ struct Timeline {
     let post_image: String
 }
 
-let timelines: [Timeline] = [
-    Timeline(id: 0, name: "Arupaka", image: "flower", post: "This is post content", post_image: "flower"),
-    Timeline(id: 1, name: "Buta", image: "flower", post: "This is post content", post_image: "flower"),
-    Timeline(id: 2, name: "Hamster", image: "flower", post: "This is post content", post_image: "flower"),
-    Timeline(id: 3, name: "Hiyoko", image: "flower", post: "This is post content", post_image: "flower"),
-    Timeline(id: 4, name: "Inu", image: "flower", post: "This is post content", post_image: "flower")
-]
-
 struct SNS: View {
     @StateObject var viewModel = PostViewModel(model: [PostModel()])
-    @State var testArr:[String] = []
     
     var body: some View {
             VStack() {
