7eb6117c37f1bc4c0cfa4ca73e308c250d70127d
takumi0512 <115136152+takumi0512@users.noreply.github.com>
2023-02-05 17:12:54 +0900

    edit nikki

--- zoqs_sns/View/Nikki/NIKKI.swift
+++ zoqs_sns/View/Nikki/NIKKI.swift
@@ -11,9 +11,10 @@ struct NIKKI: View {
     
     @State var text: String = ""
     
-    
     @State private var image: UIImage?
     @State var showingImagePicker = false
+    
+    let ScreenWidth = (UIScreen.main.bounds.width)*0.9
     
     
     var body: some View {
@@ -25,31 +26,114 @@ struct NIKKI: View {
             
             
             //メイン
-            VStack {
-                Text("Write today's memory")
-                    .font(.system(size:30,weight: .heavy))
-                    .foregroundColor(.white)
-                // 入力
-                TextEditor(text: $text)
-                    .frame(height: 400)
-                    .padding()
-                
-                
-                HStack{
-                    Button(action: {
-                        showingImagePicker = true
-                    }) {
-                        Image(systemName: "photo")
-                            .resizable()
-                            .scaledToFit()//縦横比維持
-                            .frame(width: 100)
+            ScrollView{
+                VStack {
+                    Text("Write today's memory")
+                        .font(.system(size:30,weight: .heavy))
+                        .foregroundColor(.white)
+                    
+                    //feeling入力画面
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(.white)
+                            .frame(width: ScreenWidth)
+                        VStack{
+                            Text("今日はどんな一日でしたか？").font(.headline)
+                            HStack{
+                                ForEach(0..<5, id: \.self) { index in
+                                    Spacer()
+                                    Button(action: {
+                                        print("click")
+                                    }, label: {
+                                        Image(systemName: "face.smiling")
+                                            .resizable()
+                                            .frame(width: 30 ,height: 30)
+                                    })
+                                }//foreach
+                                Spacer()
+                            }//hstack
+                        }.padding()
+                    }//zstack
+                    
+                    
+                    //feeling2
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(.white)
+                            .frame(width: ScreenWidth)
+                        VStack{
+                            Text("今日の気分は？").font(.headline)
+                            ForEach(0..<3, id: \.self){ index in
+                                HStack{
+                                    ForEach(0..<5, id: \.self) { index in
+                                        VStack{
+                                            Button(action: {
+                                                print("click")
+                                            }, label: {
+                                                Image(systemName: "face.smiling")
+                                                    .resizable()
+                                                    .frame(width: 30 ,height: 30)
+                                            })
+                                            Text("楽しい")
+                                        }//vstack item
+                                    }//foreach
+                                }//hstack
+                            }//foreach
+                        }.padding()
+                    }//zstack
+                    
+                    
+                    //meet
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundColor(.white)
+                            .frame(width: ScreenWidth)
+                        VStack{
+                            Text("誰といた？").font(.headline)
+                            HStack{
+                                ForEach(0..<5, id: \.self) { index in
+                                    Spacer()
+                                    VStack{
+                                        Button(action: {
+                                            print("click")
+                                        }, label: {
+                                            Image(systemName: "face.smiling")
+                                                .resizable()
+                                                .frame(width: 30 ,height: 30)
+                                        })
+                                        Text("友達")
+                                    }
+                                }//foreach
+                                Spacer()
+                            }//hstack
+                        }.padding()
+                    }//zstack
+                    
+                    
+                    // 入力
+                    TextEditor(text: $text)
+                        .frame(width: ScreenWidth, height: 200)
+                        .padding()
+                        
+                    
+                    
+                    HStack{
+                        Button(action: {
+                            showingImagePicker = true
+                        }) {
+                            Image(systemName: "photo")
+                                .resizable()
+                                .scaledToFit()//縦横比維持
+                                .frame(width: 100)
+                                .background(Color.white)
+                        }
+                    }
+                    .sheet(isPresented: $showingImagePicker) {
+                        ImagePicker(sourceType: .photoLibrary, selectedImage: $image)
                     }
                 }
-                .sheet(isPresented: $showingImagePicker) {
-                    ImagePicker(sourceType: .photoLibrary, selectedImage: $image)
-                }
-            }
-        }
+            }//scrollview
+        }//zstack
         
         
         
